39d4d85246fbdb5ef851d718338d5db5c04dfa60
So-far-Away <viscun.catalin@uni-weimar.de>
2024-06-29 23:43:08 +0200

    Implementierung von ::print

--- framework/shape.hpp
+++ framework/shape.hpp
@@ -8,12 +8,24 @@ public:
     std::string name_;
     std::string color_;
 
-    Shape(const std::string& name, const std::string& color)
+    Shape(const std::string& name, const std::string& color) 
         : name_(name), color_(color) {}
 
     virtual double area() const = 0;
     virtual double volume() const = 0;
     virtual ~Shape() {}//das ist en virtueller Destruktor
+
+    virtual std::ostream& print(std::ostream& os) const {
+        os << "Shape(Name: " << name_ << ", Color: " << color_ << ")";
+        return os;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Shape& s);
 };
+
+// Implementierung des Ausgabeoperators außerhalb der Klassendefinition
+inline std::ostream& operator<<(std::ostream& os, const Shape& s) {
+    return s.print(os);
+}
 
 #endif // SHAPE_H
