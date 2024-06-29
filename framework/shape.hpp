8ff5d0a6fea8cd42032637ad28d992eca192c632
So-far-Away <viscun.catalin@uni-weimar.de>
2024-06-29 15:30:00 +0200

    name und color hinzugefügt

--- framework/shape.hpp
+++ framework/shape.hpp
@@ -1,8 +1,16 @@
 #ifndef SHAPE_H
 #define SHAPE_H
+#include <string>
 
 class Shape {
 public:
+
+    std::string name_;
+    std::string color_;
+
+    Shape(const std::string& name, const std::string& color)
+        : name_(name), color_(color) {}
+
     virtual double area() const = 0;
     virtual double volume() const = 0;
     virtual ~Shape() {}//das ist en virtueller Destruktor
--- source/tests.cpp
+++ source/tests.cpp
@@ -6,16 +6,20 @@
 
 // Tests for Sphere
 TEST_CASE("Sphere calculates area and volume correctly", "[Sphere]") {
-    Sphere sphere(glm::vec3(0.0, 0.0, 0.0), 1.0);
+    Sphere sphere("Test Sphere", "Red", glm::vec3(0.0, 0.0, 0.0), 2.0);
 
+    REQUIRE(sphere.name_ == "Test Sphere");
+    REQUIRE(sphere.color_ == "Red");
     REQUIRE(Approx(sphere.area()).epsilon(0.001) == 12.566370614359172);//4*pi*r^2
     REQUIRE(Approx(sphere.volume()).epsilon(0.001) == 4.1887902047863905);//(4/3)*pi*r^3
 }
 
 // Tests for Box
 TEST_CASE("Box calculates area and volume correctly", "[Box]") {
-    Box box(glm::vec3(0.0, 0.0, 0.0), glm::vec3(1.0, 1.0, 1.0));
+    Box box("Test Box", "Blue", glm::vec3(0.0, 0.0, 0.0), glm::vec3(1.0, 2.0, 3.0));
 
+    REQUIRE(box.name_ == "Test Box");
+    REQUIRE(box.color_ == "Blue");
     REQUIRE(Approx(box.area()).epsilon(0.001) == 6.0);// 2*(1*1 + 1*1 + 1*1)
     REQUIRE(Approx(box.volume()).epsilon(0.001) == 1.0);//1*1*1
 }
